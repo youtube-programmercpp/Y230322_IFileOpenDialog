1d8786f55a1209fe997aff7f0a04de01a87ce0df
cat <sbl908k@yahoo.co.jp>
2023-03-26 09:09:17 +0900

    SHBrowseForFolderWの使用例を追加

--- WindowsProject1/WindowsProject1.cpp
+++ WindowsProject1/WindowsProject1.cpp
@@ -3,14 +3,15 @@
 #include <Windows.h>
 #include <ShlObj.h>
 #include <memory>
-std::unique_ptr<wchar_t, decltype(CoTaskMemFree)*> フォルダ選択(HWND hwndOwner)
+#include <array>
+std::unique_ptr<wchar_t, decltype(CoTaskMemFree)*> フォルダ選択(HWND hDlg)
 {
 	std::unique_ptr<wchar_t, decltype(CoTaskMemFree)*> retval{ nullptr, CoTaskMemFree };
 	IFileDialog* dlg;
 	auto hr = CoCreateInstance(__uuidof(FileOpenDialog), nullptr, CLSCTX_INPROC_SERVER, __uuidof(dlg), (void**)&dlg);
 	if (SUCCEEDED(hr)) {
 		if (SUCCEEDED(hr = dlg->SetOptions(FOS_PICKFOLDERS))) {
-			if ((hr = dlg->Show(hwndOwner)) == S_OK) {
+			if ((hr = dlg->Show(hDlg)) == S_OK) {
 				IShellItem* item;
 				if (SUCCEEDED(hr = dlg->GetResult(&item))) {
 					LPWSTR pszName;
@@ -23,19 +24,74 @@ std::unique_ptr<wchar_t, decltype(CoTaskMemFree)*> フォルダ選択(HWND hwndO
 	}
 	return retval;
 }
+
+std::unique_ptr<wchar_t, decltype(CoTaskMemFree)*> フォルダ選択2(HWND hDlg)
+{
+	UINT ulFlags = 0;
+#define	Macro(name, value)  if (IsDlgButtonChecked(hDlg, IDC_CHECK_##name)) ulFlags |= name;
+#include "BIF.h"
+#undef	Macro
+	std::unique_ptr<wchar_t, decltype(CoTaskMemFree)*> retval{ nullptr, CoTaskMemFree };
+	wchar_t szDisplayName[MAX_PATH];
+	*szDisplayName = L'\0';
+	if (const std::unique_ptr<std::remove_pointer_t<PIDLIST_ABSOLUTE>, decltype(CoTaskMemFree)*> pidl
+		{ /*SHSTDAPI_(PIDLIST_ABSOLUTE)*/SHBrowseForFolderW(/*_In_ LPBROWSEINFOW lpbi*/std::array< BROWSEINFOW, 1>
+			{ /*HWND              hwndOwner     ;*/hDlg
+			, /*PCIDLIST_ABSOLUTE pidlRoot      ;*/nullptr 
+			, /*LPWSTR            pszDisplayName;*/szDisplayName// Return display name of item selected.
+			, /*LPCWSTR           lpszTitle     ;*/nullptr // text to go in the banner over the tree.
+			, /*UINT              ulFlags       ;*/ulFlags       // Flags that control the return stuff
+			, /*BFFCALLBACK       lpfn          ;*/nullptr 
+			, /*LPARAM            lParam        ;*/0 // extra info that's passed back in callbacks
+			, /*int               iImage        ;*/0 // output var: where to return the Image index.
+			}.data())
+		, CoTaskMemFree
+		}) {
+		wchar_t pszPath[MAX_PATH];
+		if (/*_Success_(return != 0) SHSTDAPI_(BOOL)*/SHGetPathFromIDListW
+		( /*_In_                   PCIDLIST_ABSOLUTE pidl   */pidl.get()
+		, /*_Out_writes_(MAX_PATH) LPWSTR            pszPath*/pszPath
+		)) {
+			const auto cb = (lstrlenW(pszPath) + 1) * sizeof *pszPath;
+			if (const auto pszRetVal = static_cast<LPWSTR>(CoTaskMemAlloc(cb))) {
+				retval.reset(pszRetVal);
+				CopyMemory(pszRetVal, pszPath, cb);
+			}
+		}
+	}
+	return retval;
+}
+void EnableWindow_IDC_CHECK_BIF_XXX(HWND hDlg)
+{
+	const auto bEnable = IsDlgButtonChecked(hDlg, IDC_RADIO_SHBrowseForFolder);
+#define	Macro(name, value)  EnableWindow(GetDlgItem(hDlg, IDC_CHECK_##name), bEnable); 
+#include "BIF.h"
+#undef	Macro
+	const auto bNewDialogStyle = IsDlgButtonChecked(hDlg, IDC_CHECK_BIF_NEWDIALOGSTYLE);
+	EnableWindow(GetDlgItem(hDlg, IDC_CHECK_BIF_STATUSTEXT), bEnable && !bNewDialogStyle);
+	EnableWindow(GetDlgItem(hDlg, IDC_CHECK_BIF_UAHINT    ), bEnable &&  bNewDialogStyle);
+	EnableWindow(GetDlgItem(hDlg, IDC_CHECK_BIF_SHAREABLE ), bEnable &&  bNewDialogStyle);
+}
 INT_PTR CALLBACK DialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message) {
 	case WM_INITDIALOG:
+		CheckDlgButton(hDlg, IDC_RADIO_FileOpenDialog, BST_CHECKED);
+		EnableWindow_IDC_CHECK_BIF_XXX(hDlg);
 		return true;
 	case WM_COMMAND:
 		switch (LOWORD(wParam)) {
 		case IDC_BUTTON_Folder:
-			if (const auto フォルダのパス = フォルダ選択(hDlg))
+			if (const auto フォルダのパス = IsDlgButtonChecked(hDlg, IDC_RADIO_FileOpenDialog) ? フォルダ選択(hDlg) : フォルダ選択2(hDlg) )
 				SetDlgItemTextW(hDlg, IDC_EDIT_SelectedFolder, フォルダのパス.get());
 			return true;
 		case IDCANCEL:
 			EndDialog(hDlg, IDOK);
+			return true;
+		case IDC_RADIO_FileOpenDialog          :
+		case IDC_RADIO_SHBrowseForFolder       :
+		case IDC_CHECK_BIF_NEWDIALOGSTYLE      :
+			EnableWindow_IDC_CHECK_BIF_XXX(hDlg);
 			return true;
 		default:
 			return false;
--- WindowsProject1/resource.h
+++ WindowsProject1/resource.h
@@ -9,8 +9,25 @@
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG1                     129
 #define IDC_BUTTON_Folder               1001
-#define IDC_EDIT_SelectedFolder1        1002
 #define IDC_EDIT_SelectedFolder         1002
+#define IDC_RADIO_FileOpenDialog        1003
+#define IDC_RADIO_SHBrowseForFolder     1004
+#define IDC_CHECK_BIF_RETURNONLYFSDIRS    1005
+#define IDC_CHECK_BIF_DONTGOBELOWDOMAIN   1006
+#define IDC_CHECK_BIF_STATUSTEXT          1007
+#define IDC_CHECK_BIF_RETURNFSANCESTORS   1008
+#define IDC_CHECK_BIF_EDITBOX             1009
+#define IDC_CHECK_BIF_VALIDATE            1010
+#define IDC_CHECK_BIF_NEWDIALOGSTYLE      1011
+#define IDC_CHECK_BIF_BROWSEINCLUDEURLS   1012
+#define IDC_CHECK_BIF_UAHINT              1013
+#define IDC_CHECK_BIF_NONEWFOLDERBUTTON   1014
+#define IDC_CHECK_BIF_NOTRANSLATETARGETS  1015
+#define IDC_CHECK_BIF_BROWSEFORCOMPUTER   1016
+#define IDC_CHECK_BIF_BROWSEFORPRINTER    1017
+#define IDC_CHECK_BIF_BROWSEINCLUDEFILES  1018
+#define IDC_CHECK_BIF_SHAREABLE           1019
+#define IDC_CHECK_BIF_BROWSEFILEJUNCTIONS 1020
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -20,7 +37,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
